a3c2bf11819ce9c5641a87325262b49a3392ede7
Michael Probber <mp@coinbase.com>
2020-07-27 16:08:25 -0700

    feat!: Add react-browser-pathname package

---
+++ packages/react-browser-pathname/src/index.tsx
@@ -0,0 +1,70 @@
+import React, { useContext, useCallback, useEffect, useState } from 'react';
+import type { ReactNode } from 'react';
+
+const PathnameContext = React.createContext<string | null>(null);
+const UpdateContext = React.createContext(() => {
+  return;
+});
+
+const getPathname = () => `${window.location.pathname}`;
+
+const BrowserPathname = ({ children }: { children: ReactNode }) => {
+  const [location, setLocation] = useState(getPathname());
+
+  const setCurrentBrowserPathname = useCallback(() => {
+    setLocation(getPathname());
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('popstate', setCurrentBrowserPathname);
+
+    return () => {
+      window.removeEventListener('popstate', setCurrentBrowserPathname);
+    };
+  }, [setCurrentBrowserPathname]);
+
+  return (
+    <UpdateContext.Provider value={setCurrentBrowserPathname}>
+      <PathnameContext.Provider value={location}>
+        {children}
+      </PathnameContext.Provider>
+    </UpdateContext.Provider>
+  );
+  return React.createElement(
+    UpdateContext.Provider,
+    { value: setCurrentBrowserPathname },
+    React.createElement(
+      PathnameContext.Provider,
+      { value: location },
+      children,
+    ),
+  );
+};
+
+export const useBrowserPathname = () => useContext(PathnameContext);
+
+export const usePushPath = () => {
+  const setCurrentBrowserPathname = useContext(UpdateContext);
+
+  return useCallback(
+    (path: string, title: string | undefined = '') => {
+      window.history.pushState({}, title, path);
+      setCurrentBrowserPathname();
+    },
+    [setCurrentBrowserPathname],
+  );
+};
+
+export const useReplacePath = () => {
+  const setCurrentBrowserPathname = useContext(UpdateContext);
+
+  return useCallback(
+    (path: string, title = '') => {
+      window.history.replaceState({}, title, path);
+      setCurrentBrowserPathname();
+    },
+    [setCurrentBrowserPathname],
+  );
+};
+
+export default BrowserPathname;
